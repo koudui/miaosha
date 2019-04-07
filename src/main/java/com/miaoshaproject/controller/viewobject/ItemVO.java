940bbdcfa7ca141d07618c4e5e92e78e7f08233a
koudui <1909140986@qq.com>
2019-04-07 21:36:47 +0800

    完成秒杀模块的前端设计，难点在于倒计时的处理；修正PromoDOMapper.xml中关于不能在数据库中插入end_date的bug；将ItemVO中的startDate类型修改为字符串，解决不能在前端页面正常显示的bug。

--- src/main/java/com/miaoshaproject/controller/viewobject/ItemVO.java
+++ src/main/java/com/miaoshaproject/controller/viewobject/ItemVO.java
@@ -1,7 +1,5 @@
 package com.miaoshaproject.controller.viewobject;
 
-import org.joda.time.DateTime;
-
 import java.math.BigDecimal;
 
 /**
@@ -44,7 +42,7 @@ public class ItemVO {
     private Integer promoId;
 
     //秒杀活动的开始时间，用于做即将开始秒杀活动的倒计时
-    private DateTime startDate;
+    private String startDate;
 
     public Integer getPromoStatus() {
         return promoStatus;
@@ -70,11 +68,11 @@ public class ItemVO {
         this.promoId = promoId;
     }
 
-    public DateTime getStartDate() {
+    public String getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(DateTime startDate) {
+    public void setStartDate(String startDate) {
         this.startDate = startDate;
     }
 
